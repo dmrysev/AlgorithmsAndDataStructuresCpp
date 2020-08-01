395815dc8799d4031bb5b49d65797c38ad9b8c8e
dmrysev <dmrysev@gmail.com>
2020-08-01 13:43:46 +0300

    Removed my_static_lib

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include "my_static_lib/addition.hpp"
 #include "my_dynamic_lib/multiplication.hpp"
 
 using namespace AlgorithmsAndDataStructures;
 
 int main() {
-    double result = MyStaticLib::Addition::add2(2);
-    std::cout << "2 + 2 = " << result << std::endl;
-
     double result2 = MyDynamicLib::Multiplication::multBy2(3);
     std::cout << "3 * 2 = " << result2 << std::endl;
 
--- core/my_static_lib/include/my_static_lib/addition.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace AlgorithmsAndDataStructures::MyStaticLib::Addition {
-
-double add2(double input);
-
-}
