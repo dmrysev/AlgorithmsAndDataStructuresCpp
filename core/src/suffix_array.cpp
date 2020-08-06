c7d50d915402b8cfa6308509a813311ba8d69501
dmrysev <dmrysev@gmail.com>
2020-08-06 19:15:54 +0300

    Implemented initLongestCommonPrefixArray

--- core/src/suffix_array.cpp
+++ core/src/suffix_array.cpp
@@ -20,11 +20,23 @@ std::vector<size_t> initLongestCommonPrefixArray(
     const std::vector<size_t>& suffixArray,
     const std::string& str)
 {
-    for(auto it = suffixArray.begin(); it < suffixArray.end() - 1; it += 2) {
-        std::string strA = str.substr(*it, str.size());
-        std::string strB = str.substr(*(it + 1), str.size());
+    std::vector<size_t> lcpArray(suffixArray.size());
+    lcpArray[0] = 0;
+    size_t lcpArrayIndex = 1;
+    for(auto it = suffixArray.begin(); it < suffixArray.end() - 1; it++) {
+        size_t i = 0;
+        size_t indexA = *(it);
+        size_t indexB = *(it + 1);
+        size_t min = std::min(str.size() - indexA, str.size() - indexB);
+        while(i < min) {
+            char a = str[indexA + i];
+            char b = str[indexB + i];
+            if(a != b) break;
+            i++;
+        }
+        lcpArray[lcpArrayIndex++] = i;
     }
-    return {};
+    return lcpArray;
 }
 
 std::vector<size_t> initLongestCommonPrefixArray(const std::string& str) {
