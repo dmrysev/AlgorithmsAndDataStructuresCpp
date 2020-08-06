0eb6ef01c778feb2bec7534e909cc19db17680d0
dmrysev <dmrysev@gmail.com>
2020-08-06 18:01:50 +0300

    Complexity test for suffix array constructin

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include "priority_queue/working_with_std_priority_queue.h"
+#include <chrono>
+#include <random>
+#include "suffix_array/construction.h"
 
+using namespace std::chrono;
 using namespace AlgorithmsAndDataStructures::Core;
 
 int main() {
-    PriorityQueue::WorkingWithStdPriorityQueue::stdPriorityQueueTest();
-
+    std::mt19937 generator{std::random_device{}()};
+    std::uniform_int_distribution<int> distribution{'a', 'z'};
+    size_t size = std::pow(10ll, 6ll);
+    std::string rand_str(size, '\0');
+    for(char& dis: rand_str) dis = static_cast<char>(distribution(generator));
+    auto begin = high_resolution_clock::now();
+    auto suffixArray = SuffixArray::Construction::initSuffixArray(rand_str);
+    auto elapsed = high_resolution_clock::now() - begin;
+    std::cout << duration_cast<milliseconds>(elapsed).count() << std::endl;
     return 0;
 }
