b2f142d405fa7150d629077e0d89c7fc74766d9b
dmrysev <dmrysev@gmail.com>
2020-08-07 15:15:54 +0300

    Code refactored

--- core/src/suffix_array.cpp
+++ core/src/suffix_array.cpp
@@ -10,11 +10,11 @@ SuffixArray initSuffixArray(const std::string& str) {
         return std::lexicographical_compare(str.begin() + a, str.end(),
                                             str.begin() + b, str.end());
     };
-    std::set<size_t, decltype(compare)> suffices(compare);
+    std::set<size_t, decltype(compare)> suffixes(compare);
     for(size_t i = 0; i < str.size(); i++) {
-        suffices.insert(i);
+        suffixes.insert(i);
     }
-    return {suffices.begin(), suffices.end()};
+    return {suffixes.begin(), suffixes.end()};
 }
 
 LongestCommonSuffixArray initLongestCommonPrefixArray(
