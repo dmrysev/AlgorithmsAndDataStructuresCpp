c5be3ff995acf2dce010badf1fb7c7a8dadd9dcc
Dmitri <dmrysev@gmail.com>
2020-08-24 13:05:22 +0300

    Added implementation of findSmallestPositiveMissingInteger

--- core/src/algorithm/numeric.cpp
+++ core/src/algorithm/numeric.cpp
@@ -1,10 +1,17 @@
 #include "algorithm/numeric.h"
-
+#include <set>
 
 namespace Algorithm::Numeric {
 
 int findSmallestPositiveMissingInteger(const std::vector<int>& values) {
-    return 0;
+    std::set<int> valuesSorted{values.begin(), values.end()};
+    int smallest = 1;
+    for(int i: valuesSorted) {
+        if(i <= 0) continue;
+        if(smallest != i) return smallest;
+        smallest++;
+    }
+    return smallest++;
 }
 
 
